cda0b40ab142eab5b1c350c30d64f5c894275728
Tarek Ahmed <tarek5701@gmail.com>
2021-09-13 12:44:30 -0700

    Working version

--- config/media.py
+++ config/media.py
@@ -13,4 +13,4 @@ else:
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 MEDIA_URL = '/media/'   
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_root'), ]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_root'), ]
